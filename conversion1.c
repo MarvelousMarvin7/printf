a4d0fd179e64c166abe35fd5cc8f03a6bf0a84b9
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-03-27 19:12:56 +0000

    handling modifiers %s, %c and '%%'

---
+++ conversion1.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_char - prints a character
+ * @list: va_list containing the character to be printed
+ *
+ * Return: number of characters printed
+ */
+int print_char(va_list list)
+{
+	char c;
+
+	c = va_arg(list, int);
+	return (putchar(c));
+}
+/**
+ * print_string - prints a string
+ * @list: va_list containing the string to be printed
+ *
+ * Return: number of characters printed
+ */
+int print_string(va_list list)
+{
+	char *s;
+	int i = 0;
+
+	s = va_arg(list, char *);
+
+	if (s == NULL)
+		s = "(null)";
+
+	while (s[i])
+	{
+		putchar(s[i]);
+		i++;
+	}
+
+	return (i);
+}
+/**
+ * print_percent - prints a percent symbol
+ * @list: va_list (unused)
+ *
+ * Return: number of characters printed
+ */
+int print_percent(va_list list)
+{
+	(void)list;
+	return (putchar('%'));
+}
